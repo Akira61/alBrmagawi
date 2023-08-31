cce7255e648c8c3ee8fd22b7c4e53f09ec19a2a5
Akira61 <otaku.fahad11@gmail.com>
2023-08-31 20:38:34 +0300

    saving order to DB after payment

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -8,6 +8,7 @@ const {Query, asyncQuery}     = require('../server');
 const fetch     = require('node-fetch');
 const request = require('request');
 const axios = require('axios');
+const { loginRequirement } = require('../auth/basicAuth');
 
 
 class Gateway{
@@ -46,12 +47,11 @@ class Gateway{
 }
 
 
-
 router.get('/cart', (req, res) => {
     res.sendFile(path.join(__dirname + '../../views/cart.html'))
 })
 
-
+ 
 router.post('/gateway', async(req,res)=> {
 
     if(!req.body.productID) return res.status(500).send("no product sent")
@@ -68,18 +68,22 @@ router.post('/gateway', async(req,res)=> {
     const baseURL = process.env.payment_base_url;
     const token = process.env.payment_token;
     const gateway = new Gateway(baseURL, token);
- 
+    
+    // send request to gateway api to get paymentPage for the product
     const paymentPage = await gateway.sendPayment('/v2/SendPayment', {
         NotificationOption: 'LNK',
-        CustomerName: req.session.user.first_name || 'Fahad',
+        CustomerName: 'fahad',//req.session.user.first_name+' '+req.session.user.last_name,
         DisplayCurrencyIso: 'USD',
-        CustomerEmail: req.session.user.email || 'fahad@fahad.com',
+        CustomerEmail: 'fahad@fahad.com', //req.session.user.email,
+        CustomerReference : /*Date.now(),*/ 6, //req.session.user.id,
         InvoiceValue: course[0].price,
-        CallBackUrl: process.env.payment_success_url,
+        CallBackUrl: process.env.payment_success_url /*+ `?course=${productID}` */,
         ErrorUrl: process.env.payment_failed_url,
         Language: 'en',
-    })
-    console.log(paymentPage)
+        UserDefinedField : course[0].id,
+        InvoiceItems: [ { ItemName: course[0].title, Quantity: 1, UnitPrice: course[0].price,  } ],
+    }) 
+    console.log(paymentPage) 
     // check if response is success
     if(paymentPage.IsSuccess){
         const {InvoiceId} = paymentPage.Data;
@@ -88,9 +92,84 @@ router.post('/gateway', async(req,res)=> {
     }
 })
 
+
 // '/payment/success'
-router.get('/payment/success', (req,res)=> {
-    res.status(200).send('you paied successfully ✅')
+router.get('/payment/success', async(req,res)=> {
+
+    // check invoice status
+    const {paymentId} = req.query;
+    const payload = {
+        url: process.env.payment_base_url+'/v2/getPaymentStatus',
+        method : 'POST',
+        headers : {
+            Accept: 'application/json',
+                Authorization: 'Bearer '+process.env.payment_token,
+                'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'Key' : req.query.paymentId,
+            'KeyType' : 'paymentId'
+        }),
+        json : true
+    } 
+    const uri = process.env.payment_base_url+'/v2/getPaymentStatus'
+    const response = await fetch(uri,payload)
+    const invoice = await response.json();
+    console.log(invoice)
+    console.log(invoice.Data.InvoiceItems);
+    
+    // find if invoice id already exists 
+    const invoiceExists = await asyncQuery(`
+    SELECT * FROM orders WHERE payment_id=?;
+    `, [paymentId]);
+    
+    if(invoiceExists.length > 0){
+        return res.status(403).send('invoice already exists');
+    }
+
+    // if user paied and invoice not in DB insert order
+    if(invoice.IsSuccess){
+        // insert data
+        const order = await asyncQuery(`
+        INSERT INTO orders (
+            course_id,
+            user_id,
+            payment_id,
+            payment_data,
+            date
+        ) VALUES (?,?,?,?,?)
+        `, [parseInt(invoice.Data.UserDefinedField),
+            parseInt(invoice.Data.CustomerReference),
+            paymentId,
+            JSON.stringify(invoice),
+            invoice.Data.CreatedDate
+        ])
+
+        console.log(order)
+        return res.status(200).json({success:true})
+    }
+    // request({
+    //     url: process.env.payment_base_url+'/v2/getPaymentStatus',
+    //     method : 'POST',
+    //     headers : {
+    //         Accept: 'application/json',
+    //             Authorization: 'Bearer '+process.env.payment_token,
+    //             'Content-Type': 'application/json'
+    //     },
+    //     body: {
+    //         'Key' : req.query.paymentId,
+    //         'KeyType' : 'paymentId'
+    //     },
+    //     json : true
+    // }, (err, response, body)=> {
+    //     if(err) throw new Error(err);
+    //     console.log(body);
+    //     console.log(body.Data.InvoiceItems)
+    //     res.status(200).json(body)
+    // });
+
+    // success message
+    //res.status(200).json(req.query)
 })
 
 // '/payment/failed'
