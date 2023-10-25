573205c7733a0886cda4bb1f6bbe0fd9b5d198b5
mahcodes <mhmdali102@bk.ru>
2023-10-25 21:50:55 +0300

    starting the service is always better

--- frontend/src/app/(components)/ctf/AllCTFs.jsx
+++ frontend/src/app/(components)/ctf/AllCTFs.jsx
@@ -127,29 +127,6 @@ export default function AllCTFs() {
 
               <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                 <div className="flex items-center space-x-3 w-full md:w-auto">
-                  <Box>
-                    <button
-                      id="actionsDropdownButton"
-                      data-dropdown-toggle="actionsDropdown"
-                      className="flex items-center justify-center py-4 px-6"
-                      type="button"
-                    >
-                      <svg
-                        className="-ml-1 mr-1.5 w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                      >
-                        <path
-                          clipRule="evenodd"
-                          fillRule="evenodd"
-                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        />
-                      </svg>
-                      Actions
-                    </button>
-                  </Box>
                   <div
                     id="actionsDropdown"
                     className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-american-purple dark:divide-gray-600"
