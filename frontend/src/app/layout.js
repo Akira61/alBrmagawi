8a537767c4368a9318f679231c46f618152fc602
mahcodes <mhmdali102@bk.ru>
2023-10-27 21:52:07 +0300

    changed albrmagawi to ceybarani

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Albrmagawi | The one platform to improve your cybersecurity skills",
+  title: "Ceybarani | The one platform to improve your cybersecurity skills",
   description:
     "Ceybarani gives individuals, businesses and universities the tools they need to continuously improve their cybersecurity capabilities — all in one place.",
 };
